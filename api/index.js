4ec9efbae4c9e1e3642d86a60691d5f9426cc035
github username <github email address>
2019-01-17 15:33:29 -0500

    heroku changes

--- api/index.js
+++ api/index.js
@@ -2,14 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-
-router.get('/test', function(req, res, next) {
-  res.json(200, 'This is working')
-})
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // res.render('index', { title: 'Express' });
+  res.json({status: "200 this works"})
 });
-
-
 
 module.exports = router;
